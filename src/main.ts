69c9d7f6f989c69479f76b49bd23966a91d84126
zhaoqian.ipp <zhaoqian.ipp@bytedance.com>
2022-12-15 19:07:29 +0800

    feat: cacheAsBitmap

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
-import * as PIXI from 'pixi.js';
-import { Sprite, Texture } from 'pixi.js';
+import * as PIXI from "pixi.js";
+import { Texture } from "pixi.js";
 
+window.addEventListener(
+  "wheel",
+  (ev) => {
+    ev.stopPropagation();
+    // ev.preventDefault();
+  },
+  { capture: true }
+);
 
 enum PuzzleType {
   LeftTop,
@@ -21,14 +29,22 @@ enum PuzzleType {
 
 const SIZE = 64;
 
-
 async function main() {
-  const app = new PIXI.Application({ background: '#1099bb', width: 1024, height: 768 });
+  const [row, col] = [11 + 2 * 8, 7 + 2 * 8];
+  const { clientHeight, clientWidth } = window.document.body;
+  const app = new PIXI.Application({
+    background: "#1099bb",
+    width: clientWidth,
+    height: clientHeight,
+  });
   document.body.appendChild(app.view as any);
-  const content = PIXI.Texture.from('textures/content.jpeg');
+  PIXI.Assets.add("content", "textures/content.jpeg");
+  PIXI.Assets.add("puzzle", "textures/puzzle.png");
+  const assets = await PIXI.Assets.load(["puzzle", "content"]);
+  const content = assets["content"];
   // app.stage.addChild(new PIXI.Sprite(content));
   // Create a new texture
-  const texture = PIXI.Texture.from('textures/puzzle.png');
+  const texture = assets["puzzle"];
   const sheet = new PIXI.Spritesheet(texture, {
     frames: {
       [PuzzleType.LeftTop]: {
@@ -71,16 +87,14 @@ async function main() {
           w: 128,
           h: 128,
         },
-      }
+      },
     },
     meta: {
-      scale: '1',
+      scale: "1",
     },
   });
 
-
-
-  const textures = await sheet.parse()
+  const textures = await sheet.parse();
 
   const mapper: Record<PuzzleType, () => PIXI.Sprite> = {
     [PuzzleType.LeftTop]: () => {
@@ -93,7 +107,7 @@ async function main() {
       return new PIXI.Sprite(textures[PuzzleType.CenterTop1]);
     },
     [PuzzleType.RightTop]: () => {
-      const sprite = new PIXI.Sprite(textures[PuzzleType.LeftTop])
+      const sprite = new PIXI.Sprite(textures[PuzzleType.LeftTop]);
       sprite.anchor.set(0.5);
       sprite.scale.x = -1;
       return sprite;
@@ -101,13 +115,13 @@ async function main() {
     [PuzzleType.LeftCenter0]: () => {
       const sprite = new PIXI.Sprite(textures[PuzzleType.CenterTop0]);
       sprite.anchor.set(0.5);
-      sprite.rotation = - Math.PI / 2;
+      sprite.rotation = -Math.PI / 2;
       return sprite;
     },
     [PuzzleType.LeftCenter1]: () => {
       const sprite = new PIXI.Sprite(textures[PuzzleType.CenterTop1]);
       sprite.anchor.set(0.5);
-      sprite.rotation = - Math.PI / 2;
+      sprite.rotation = -Math.PI / 2;
       return sprite;
     },
     [PuzzleType.Center0]: () => {
@@ -129,7 +143,7 @@ async function main() {
       return sprite;
     },
     [PuzzleType.LeftBottom]: () => {
-      const sprite = new PIXI.Sprite(textures[PuzzleType.LeftTop])
+      const sprite = new PIXI.Sprite(textures[PuzzleType.LeftTop]);
       sprite.anchor.set(0.5);
       sprite.scale.x = -1;
       sprite.rotation = Math.PI;
@@ -153,14 +167,17 @@ async function main() {
       sprite.rotation = Math.PI;
       return sprite;
     },
-  }
+  };
 
   const sprites: PIXI.Container[] = [];
   const container = new PIXI.Container();
-  container.x = 34;
-  container.y = 34;
+  container.scale.set(0.5);
 
-  // container.scale.set(0.5);
+  // container.
+
+  const test = new PIXI.Sprite(Texture.WHITE);
+
+ 
 
   const toPuzzleType = (i: number, j: number, row: number, col: number) => {
     if (i === 0 && j === 0) {
@@ -224,46 +241,43 @@ async function main() {
         return PuzzleType.Center1;
       }
     }
-  }
+  };
 
   let dragTarget;
 
   const toPuzzle = (x: number, y: number, row: number, col: number) => {
     const sprite = mapper[toPuzzleType(x, y, row, col)]();
+    const puzzle = new PIXI.Container();
     const r = new PIXI.Container();
     sprite.anchor.set(0.5);
 
     const hit = new PIXI.Sprite(Texture.WHITE);
-    hit.tint = 0xff00ff;
     hit.alpha = 0;
     hit.width = SIZE - 8;
     hit.height = SIZE - 8;
     hit.anchor.set(0.5);
-    // hit.cursor = 'move';
 
     const contentLayer = new PIXI.Sprite(content);
-
-    const renderTexture = PIXI.RenderTexture.create({ width: 800, height: 600 });
-
-
-    // contentLayer.width = 32;
-    // contentLayer.height = 32;
 
     contentLayer.x -= 33;
     contentLayer.x -= x * SIZE;
     contentLayer.y -= 33;
     contentLayer.y -= y * SIZE;
 
+    puzzle.addChild(sprite);
+    puzzle.addChild(contentLayer);
 
-    r.addChild(sprite);
-    // r.addChild(contentLayer);
+    contentLayer.mask = sprite;
+    puzzle.cacheAsBitmap = true;
+    puzzle.hitArea = new PIXI.Rectangle(0, 0, 0, 0);
 
-
-    // contentLayer.mask = sprite;
-
+    r.addChild(puzzle);
     r.addChild(hit);
-    r.x += SIZE * x;
-    r.y += SIZE * y;
+    r.x += 34 + SIZE * x;
+    r.y += 34 + SIZE * y;
+
+    // r.x = 34 + Math.random() * 2000;
+    // r.y = 34 + Math.random() * 1600;
 
     hit.interactive = true;
     sprite.interactive = false;
@@ -272,126 +286,43 @@ async function main() {
       dragTarget = r;
       r.zIndex = -1;
       dragTarget.parent.toLocal(event.global, null, dragTarget.position);
-      app.stage.on('pointermove', onDragMove);
-    })
-
+      app.stage.on("pointermove", onDragMove);
+    });
     return r;
-  }
-  const [row, col] = [11, 7];
+  };
 
   for (let i = 0; i < row; i++)
     for (let j = 0; j < col; j++) {
       sprites.push(toPuzzle(i, j, row, col));
     }
 
-
-
-
-
   app.stage.interactive = true;
   app.stage.hitArea = app.screen;
 
-  container.addChild(...sprites)
+  container.addChild(...sprites);
+  // test.width = 100;
+  // test.height = 100;
+  // container.addChild(test);
   app.stage.addChild(container);
-  // container.pivot.x = container.width / 2;
-  // container.pivot.y = container.height / 2;
+  
 
   function onDragMove(event) {
     if (dragTarget) {
       dragTarget.parent.toLocal(event.global, null, dragTarget.position);
     }
   }
-  app.stage.on('pointerleave', onDragEnd);
-  app.stage.on('pointerup', onDragEnd);
+  app.stage.on("pointerleave", onDragEnd);
+  app.stage.on("pointerup", onDragEnd);
 
-
-
+  
 
   function onDragEnd() {
     if (dragTarget) {
-      app.stage.off('pointermove', onDragMove);
+      app.stage.off("pointermove", onDragMove);
       dragTarget.alpha = 1;
       dragTarget = null;
     }
   }
-
-  // app.stage.addChild(...sprites.map(sprite => {
-  //   const container = new PIXI.Container();
-  //   const hit = new PIXI.Sprite(Texture.WHITE);
-
-  //   hit.interactive = true;
-  //   hit.tint = 0xff00ff;
-  //   // hit.position = sprite.position;
-
-  //   hit.width = 50;
-  //   hit.height = 50;
-  //   hit.alpha = 0.5;
-  //   // hit.anchor.set(-0.75);
-  //   // hit.anchor.set(0.5);
-  //   hit.position = sprite.position;
-
-
-  //   const contentLayer = new PIXI.Sprite(content);
-  //   // container.addChild(contentLayer);
-  //   container.addChild(sprite);
-  //   container.addChild(hit);
-  //   // container.interactive = true;
-  //   // container.cursor = 'pointer';
-  //   // container.position.set(Math.random() * 200, Math.random() * 200)
-  //   container.position = sprite.position;
-  //   // container.hitArea = new PIXI.Rectangle(32, -32, 62, 62)
-  //   // container.hitArea = 
-  //   // container.width
-  //   contentLayer.position = sprite.position;
-  //   contentLayer.position.set(-sprite.position.x, -sprite.position.y);
-  //   // hit.position.set(-sprite.position.x, -sprite.position.y);
-  //   sprite.position.set(0, 0);
-  //   console.log(container.getLocalBounds());
-
-  //   // container.pivot.x = container.width / 2;
-  //   // container.pivot.y = container.height / 2;
-
-  //   // contentLayer.position.set(sprite.position.x, sprite.position.y);
-
-  //   // container.position.set(100,100);
-  //   // const bounds = container.getBounds(false);
-  //   // console.log(bounds);
-  //   // sprite.position.set(0, 0);
-
-  //   // contentLayer.mask = sprite;
-  //   // container.interactive = true;
-  //   // container.on('pointerdown', () => {
-  //   //   // dragTarget = container;
-  //   //   // container.visible = false;
-  //   //   // app.stage.on('pointermove', onDragMove);
-  //   // }, container);
-  //   // console.log(sprite.position);
-  //   return container;
-  // }));
-
-  // app.stage.on('pointerup', onDragEnd);
-  // app.stage.on('pointerupoutside', onDragEnd);
-
-  // function onDragMove(event) {
-  //   if (dragTarget) {
-  //     // console.log(event.global, dragTarget, sp.position);
-  //     dragTarget.toLocal(event.global, dragTarget, dragTarget.position);
-  //     // dragTarget.toLocal(event.global, dragTarget, sp.position);
-  //     // c.toLocal(event.global, dragTarget, sp.position);
-  //     // sp.parent.toLocal(event.global, dragTarget, sp.position);
-  //   }
-  // }
-
-  // function onDragEnd() {
-  //   if (dragTarget) {
-  //     app.stage.off('pointermove', onDragMove);
-  //     dragTarget = null;
-  //   }
-  // }
-
-  app.ticker.add(() => {
-    app.renderer.render(container);
-  })
 }
 
 main();
